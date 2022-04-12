cd215843777296b5d92e66acdb6d0f9de35b48d8
aidan <aidanhmiles@gmail.com>
2022-04-12 14:50:19 -0700

    annotates Post model using annotate gem

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  author     :string
+#  body       :string
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Post < ApplicationRecord
+  after_create_commit { broadcast_prepend_to "tweets" }
+  after_update_commit { broadcast_replace_to "tweets" }
+  after_destroy_commit { broadcast_remove_to "tweets" }
+
+  validates :body, presence: true
+
 end
