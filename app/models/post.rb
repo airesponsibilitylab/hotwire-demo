3b7044140e87e739084dfda339c9ca7798952115
aidan <aidanhmiles@gmail.com>
2022-04-12 18:14:17 -0700

    Copies in files from excid3's hotwire-twitter-clone demo, updates names where applicable (Tweet -> Post)

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,9 +10,9 @@
 #  updated_at :datetime         not null
 #
 class Post < ApplicationRecord
-  after_create_commit { broadcast_prepend_to "tweets" }
-  after_update_commit { broadcast_replace_to "tweets" }
-  after_destroy_commit { broadcast_remove_to "tweets" }
+  after_create_commit { broadcast_prepend_to "posts" }
+  after_update_commit { broadcast_replace_to "posts" }
+  after_destroy_commit { broadcast_remove_to "posts" }
 
   validates :body, presence: true
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :posts do
+    # resource :votes
+  end
+
   # Defines the root path route ("/")
   root "static_pages#home"
 end
